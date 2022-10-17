3cfecb1c6ca3e25054020dacb5fd3f1d829ed9a5
Kevin Klopfenstein <kklopfenstein@users.noreply.github.com>
2022-10-17 14:59:22 +0000

    Add default-address-pool setting

--- src/docker-in-docker/install.sh
+++ src/docker-in-docker/install.sh
@@ -12,6 +12,7 @@ DOCKER_VERSION=${VERSION:-"latest"} # The Docker/Moby Engine + CLI should match 
 USE_MOBY=${MOBY:-"true"}
 DOCKER_DASH_COMPOSE_VERSION=${DOCKERDASHCOMPOSEVERSION:-"v1"} # v1 or v2
 AZURE_DNS_AUTO_DETECTION=${AZUREDNSAUTODETECTION:-"true"}
+DOCKER_DEFAULT_ADDRESS_POOL=${DOCKERDEFAULTADDRESSPOOL}
 
 ENABLE_NONROOT_DOCKER=${ENABLE_NONROOT_DOCKER:-"true"}
 USERNAME=${USERNAME:-"automatic"}
@@ -325,7 +326,8 @@ tee /usr/local/share/docker-init.sh > /dev/null \
 
 set -e
 
-AZURE_DNS_AUTO_DETECTION=$AZURE_DNS_AUTO_DETECTION
+export AZURE_DNS_AUTO_DETECTION=${AZURE_DNS_AUTO_DETECTION}
+export DOCKER_DEFAULT_ADDRESS_POOL=${DOCKER_DEFAULT_ADDRESS_POOL}
 EOF
 
 tee -a /usr/local/share/docker-init.sh > /dev/null \
@@ -377,16 +379,24 @@ dockerd_start="$(cat << 'INNEREOF'
         echo "Not setting dockerd DNS manually."
         CUSTOMDNS=""
     fi
+
     set -e
 
+    if [ -z "$DOCKER_DEFAULT_ADDRESS_POOL" ]
+    then
+        DEFAULT_ADDRESS_POOL=""
+    else
+        DEFAULT_ADDRESS_POOL="--default-address-pool $DOCKER_DEFAULT_ADDRESS_POOL"
+    fi
+
     # Start docker/moby engine
-    ( dockerd $CUSTOMDNS > /tmp/dockerd.log 2>&1 ) &
+    ( dockerd $CUSTOMDNS $DEFAULT_ADDRESS_POOL > /tmp/dockerd.log 2>&1 ) &
 INNEREOF
 )"
 
 # Start using sudo if not invoked as root
 if [ "$(id -u)" -ne 0 ]; then
-    sudo /bin/sh -c "${dockerd_start}"
+    sudo -E /bin/sh -c "${dockerd_start}"
 else
     eval "${dockerd_start}"
 fi
